6afc37024a4d7346df3ec908d82a41833cc6c264
gitguti <gitmelgtrrz1@gmail.com>
2019-08-07 14:47:22 -0400

    colores

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,17 +2,17 @@
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
 
-        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-red-500">
             Critical
         </todo-card>
         
-         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-indigo-700">
             Major
         </todo-card>
-         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-yellow-400">
             Minor
         </todo-card>
-        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg ">
             Project 1
         </todo-card>
         
