c2aba16d8cd05c91fbd72dc2ea1ea543fe9e5f31
enderson <enderson19952009@gmail.com>
2019-08-07 13:57:34 -0400

    Boton que suma en uno

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import App from './App.vue';
 import '@/assets/css/tailwind.css';
+import Task from './components/Task';
+import Button from './components/Button';
 
 import Card from './components/Card.vue';
 
 Vue.config.productionTip = false;
 
 Vue.component('todo-card', Card);
+Vue.component('todo-button', Button);
 
 new Vue({
   render: h => h(App),
