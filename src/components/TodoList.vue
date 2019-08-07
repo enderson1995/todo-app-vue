944832f398a8e452b093b82231d58fa0abe63c83
gitguti <gitmelgtrrz1@gmail.com>
2019-08-07 14:40:27 -0400

    prueba a ver
    cambie unas cosas

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,13 +2,22 @@
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
 
-        <todo-card class="mx-auto mt-4 h-64 w-full max-w-lg">
-            Todo List
+        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+            Critical
         </todo-card>
+        
+         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+            Major
+        </todo-card>
+         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+            Minor
+        </todo-card>
+        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg">
+            Project 1
+        </todo-card>
+        
 
-        <todo-card class="mx-auto mt-10 w-full max-w-lg">
-            Create/Edit Task
-        </todo-card>
+        
     </div>
 </template>
 
