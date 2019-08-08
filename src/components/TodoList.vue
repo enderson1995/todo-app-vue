a7ede894c1b61520ac3111e6ac2a8a27d0a225d0
enderson <enderson19952009@gmail.com>
2019-08-08 13:07:08 -0400

    comentado

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,22 +4,25 @@
 
         <todo-card class="mx-auto mt-4 h-16 w-full max-w-lg">
             <div v-for="tasktest in tasks" :key="tasktest.id" >
+                <!-- mostrar data -->
                 <p v-if="!editing">
                 es la tarea {{tasktest.id}} se hace {{tasktest.description}},esta:{{tasktest.done}}
                 </P>
                 <p v-else>
                     <input @keydown.enter="addTodo(tasktest.id)" v-model="edicion" placeholder="edit task" class="bg-gray-500 ">
                 </p>
+                <!-- boton de borrar -->
                 <button @click="borrar(tasktest.id)" class="bg-red-700 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">
                     X
                 </button>
+                <!-- boton de editar -->
                 <button @click="editar(tasktest.id)" class="mx-auto mt-2 bg-green-700 hover:bg-green-200 text-white font-bold py-2 px-4 rounded">
                     edit
                 </button>
             </div>
            
         </todo-card>
-       
+       <!-- otro componente -->
         <todo-card class="mx-auto mt-16 w-full max-w-lg" >
             Create/Edit Task 
             <todo-task class="mx-auto mt-16  w-full max-w-lg" @messages='recive' ></todo-task>       
