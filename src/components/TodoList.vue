497ff724954275eb9caa614515201e87ea5a4c40
enderson <enderson19952009@gmail.com>
2019-08-07 22:58:08 -0400

    Cuadricula Basica
    Division por prioridad mas espacio en el cual colocar la info.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,21 +1,41 @@
 <template>
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
-        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App</h1>
-
-        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-red-500">
+        <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App Proyecto 1</h1>
+    <div class="bg-gray-300">
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-40 max-w-lg bg-red-500">
             Critical
         </todo-card>
-        
-         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-indigo-700">
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-full max-w-lg bg-red-100">
+            Tarea
+        </todo-card>
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-auto max-w-lg bg-red-100">
+            BOTONES
+        </todo-card>
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-40 max-w-lg bg-indigo-700">
             Major
         </todo-card>
-         <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg bg-yellow-400">
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-full max-w-lg bg-red-100">
+            Tarea
+        </todo-card>
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-auto max-w-lg bg-red-100">
+            BOTONES
+        </todo-card>
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-40 max-w-lg bg-yellow-400">
             Minor
         </todo-card>
-        <todo-card class="mx-32 mt-4 h-64 w-full max-w-lg ">
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-full max-w-lg bg-red-100">
+            Tarea
+        </todo-card>
+        <todo-card class="inline-block mx-10 mt-10 h-48 w-auto max-w-lg bg-red-100">
+            BOTONES
+        </todo-card>
+    </div>
+    
+    <div>    
+        <todo-card class="inline-block mx-32 mt-4 h-64 w-full max-w-lg ">
             Project 1
         </todo-card>
-        
+    </div>      
 
         
     </div>
