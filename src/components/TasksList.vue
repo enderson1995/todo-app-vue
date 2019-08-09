a03006e3487bcbfb7e0c8f13ecaa0a6bc5517334
enderson <enderson19952009@gmail.com>
2019-08-09 11:10:36 -0400

    ir de padre a hijo

--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,16 +1,38 @@
 <template>
-    <h1 class="text-red-400">Tasks List</h1>
+    <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
+    <h1 class="text-red-900">Tasks List</h1>
+    <div v-for="(tasktest, index) in tasks" :key="tasktest.id" >
+                <!-- mostrar data -->
+                <input type="checkbox" v-model="checked" @click="tasktest.done=checked">
+                <p v-show="!tasktest.editing">{{index +1}}-) {{tasktest.title}}  {{checked}}</p>
+                <input v-model="tasktest.title" v-show="tasktest.editing" @keydown.enter="tasktest.editing=false">
+                
+                <!-- boton de borrar -->
+                <button @click="borrar(tasktest.id)" class="bg-red-700 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">
+                    X
+                </button>
+                <!-- boton de editar -->
+                <button @click="tasktest.editing=true" class="mx-auto mt-2 bg-green-700 hover:bg-green-200 text-white font-bold py-2 px-4 rounded">
+                    edit
+                </button>
+            </div>
+
+    
+    </div>
 </template>
 
 <script>
 export default {
     props: {
-
+        tasks:{
+            type: Object,
+            required: true
+        }
     },
 
     data () {
         return {
-
+            tasktest:''
         };
     },
 
@@ -19,7 +41,13 @@ export default {
     },
 
     methods: {
-
+        borrar(idRemove){
+                this.tasks=this.tasks.filter(todo => {
+                    return todo.id !== idRemove
+                }), 
+                this.$emit('eraser',this.$props.tasks)  
+            },
+       
     }
 }
 </script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,30 +2,34 @@
     <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-200">Todo App </h1>
 
-        <todo-card class="mx-auto mt-4 h-16 w-full max-w-lg">
-            <div v-for="(tasktest, index) in tasks" :key="tasktest.id" >
-                <!-- mostrar data -->
-                <input type="checkbox" v-model="checked" @click="tasktest.done=checked">
-                <p v-show="!tasktest.editing">{{index +1}}-) {{tasktest.title}}  {{checked}}</p>
-                <input v-model="tasktest.title" v-show="tasktest.editing" @keydown.enter="tasktest.editing=false">
-                
-                <!-- boton de borrar -->
-                <button @click="borrar(tasktest.id)" class="bg-red-700 hover:bg-red-200 text-white font-bold py-2 px-4 rounded">
-                    X
-                </button>
-                <!-- boton de editar -->
-                <button @click="tasktest.editing=true" class="mx-auto mt-2 bg-green-700 hover:bg-green-200 text-white font-bold py-2 px-4 rounded">
-                    edit
-                </button>
-            </div>
-           
+        <todo-card class="mx-auto mt-16 w-full max-w-lg">
+            <todo-tasksList :tasks='tasks' @eraser='eraser'>
+
+            </todo-tasksList>           
         </todo-card>
        <!-- otro componente -->
         <todo-card class="mx-auto mt-16 w-full max-w-lg" >
             Create/Edit Task 
-            <todo-task class="mx-auto mt-16  w-full max-w-lg" @messages='recive' ></todo-task>       
+        <todo-task class="mx-auto mt-16  w-full max-w-lg" @messages='recive' ></todo-task>       
         </todo-card>
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div class="container mx-auto px-4 py-20 flex flex-col flex-wrap">
         <h1 class="font-hairline text-6xl text-center text-gray-500">Todo App Proyecto 1</h1>
                 <h1 class="font-hairline text-6xl text-center text-gray-500">Proyecto 1</h1>
@@ -115,8 +119,8 @@ export default {
 
     methods: {
             recive(message){
-                this.mess=message, 
-                this.counter++  
+                this.mess=message; 
+                this.counter++;
                 this.tasks.push({
                     id:this.counter,
                     title:message,
@@ -124,15 +128,11 @@ export default {
                     editing:false
                 })           
             },
-            borrar(idRemove){
-                this.tasks=this.tasks.filter(todo => {
-                    return todo.id !== idRemove
-                })   
+            eraser(task){
+                this.tasks=task
             },
-            editar(){
-            tasktest.editing=true
-            return tasktest.editing
-            }   
+            
+             
     }
 }
 </script>
